9664bf48192bae661087ec953e3dcbe8a3db307f
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-03-24 18:51:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if index[board] == true
+  if index[board] == (0-8)
     return true/false
   end
     ## if index is between 0-8 and spot isnt taken its valid 
